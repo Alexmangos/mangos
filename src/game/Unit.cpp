13f73592d2cbaaa3d4ef99bce2482e743832d28e
Alexmangos <acore@mail.ru>
2009-11-19 22:33:54 +0600

    [patch]Fix: Prevent Hungering Cold aura remove at damage taken from diseases

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6999,6 +6999,14 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura* triggeredByAu
                 triggered_spell_id = 49772;
                 break;
             }
+			      // Hungering Cold
+            if (dummySpell->SpellIconID == 2797)
+            {
+               // Don't remove stun aura at damage taken from diseases
+                if (GetAllSpellMechanicMask(procSpell) & (1 << MECHANIC_INFECTED))
+                    return false;
+                break;
+            }
             break;
         }
         default:
@@ -12508,6 +12516,7 @@ void Unit::ProcDamageAndSpellFor( bool isVictim, Unit * pTarget, uint32 procFlag
                 DealSpellDamage(&damageInfo, true);
                 break;
             }
+			case SPELL_AURA_MOD_STUN:
             case SPELL_AURA_MOD_DAMAGE_PERCENT_TAKEN:
             case SPELL_AURA_MOD_DAMAGE_PERCENT_DONE:
             case SPELL_AURA_MANA_SHIELD:
