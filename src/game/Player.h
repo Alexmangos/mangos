0901ecb563bf4d51532cd9bec90ff9fffd95a245
Alexmangos <acore@mail.ru>
2009-11-19 22:44:33 +0600

    [patch]Barbershop - skin tone

--- src/game/Player.h
+++ src/game/Player.h
@@ -1090,7 +1090,7 @@ class MANGOS_DLL_SPEC Player : public Unit
         std::string afkMsg;
         std::string dndMsg;
 
-        uint32 GetBarberShopCost(uint8 newhairstyle, uint8 newhaircolor, uint8 newfacialhair);
+        uint32 GetBarberShopCost(uint8 newhairstyle, uint8 newhaircolor, uint8 newfacialhair, BarberShopStyleEntry const* newSkin=NULL);
 
         PlayerSocial *GetSocial() { return m_social; }
 
