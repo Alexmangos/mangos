21d71aedafbca001cb4de5b3e1b6338a30f9acc2
Alexmangos <acore@mail.ru>
2009-11-19 21:58:02 +0600

    [patch]Delayed rune convert

---
+++ addition/[patch]delayed rune convert.sql
@@ -0,0 +1,11 @@
+-- (54639) Blood of the North (Rank 1)
+DELETE FROM `spell_proc_event` WHERE `entry` IN (54639);
+INSERT INTO `spell_proc_event` VALUES (54639, 0x00, 15, 0x00400000, 0x00010000, 0x00000000, 0x00010010, 0x00000000, 0.000000, 0.000000, 0);
+
+-- (49467) Death Rune Mastery ()
+DELETE FROM `spell_proc_event` WHERE `entry` IN (49467);
+INSERT INTO `spell_proc_event` VALUES (49467, 0x00, 15, 0x00000010, 0x00020000, 0x00000000, 0x00010010, 0x00000000, 0.000000, 0.000000, 0);
+
+-- (49208) Reaping (Rank 1)
+DELETE FROM `spell_proc_event` WHERE `entry` IN (49208);
+INSERT INTO `spell_proc_event` VALUES (49208, 0x00, 15, 0x00400000, 0x00010000, 0x00000000, 0x00010010, 0x00000000, 0.000000, 0.000000, 0);
