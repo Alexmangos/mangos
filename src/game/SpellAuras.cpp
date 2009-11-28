c8e0d08852850ab5b4d8b75ab6ce7e33beaeeaaf
Insider <insider42rus@yandex.ru>
2009-11-28 15:35:07 +0700

    Reverted [patch 156]

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -6634,7 +6634,7 @@ void Aura::HandleSchoolAbsorb(bool apply, bool Real)
             m_target->RemoveAurasDueToSpell(remove_spell);
         }
 
-        /*// Ice Barrier (remove effect from Shattered Barrier)
+        // Ice Barrier (remove effect from Shattered Barrier)
         if (m_spellProto->SpellIconID == 32 && m_spellProto->SpellFamilyName == SPELLFAMILY_MAGE)
         {
             if (!((m_removeMode == AURA_REMOVE_BY_DEFAULT && !m_modifier.m_amount) || m_removeMode == AURA_REMOVE_BY_DISPEL))
@@ -6713,7 +6713,7 @@ void Aura::HandleSchoolAbsorb(bool apply, bool Real)
                     }
                 }
             }
-        }*/
+        }
     }
 }
 
